b988f1d9027be1e8b47d2268ead14bca9e2bdda2
stpn94 <79563672+stpn94@users.noreply.github.com>
2023-08-26 19:52:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+
+button(username="JongYoon", floating=True, width=221)
 
 #제목
 st.title("ChatPDF")
 st.write("---")
+
+#OpenAI KEY 입력 받기
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 #파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
@@ -40,7 +46,7 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     #Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
@@ -51,7 +57,7 @@ if uploaded_file is not None:
 
     if st.button('질문하기'):
         with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
             qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
             result = qa_chain({"query": question})
             st.write(result["result"])

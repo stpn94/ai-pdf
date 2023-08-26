4137636fcff443c2ef71a6294956af1c1c91eaef
stpn94 <79563672+stpn94@users.noreply.github.com>
2023-08-26 19:23:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
